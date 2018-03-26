108ab5d46c25628ded65b05ad8bbc5a94a4c58b4
xietf <a826597263@hotmail.com>
2018-03-26 23:07:56 +0800

    更新代码

--- src/utils/Authorized.js
+++ src/utils/Authorized.js
@@ -1,6 +1,7 @@
 import RenderAuthorized from '../components/Authorized';
 import { getAuthority } from './authority';
 
+/** 取得授权后的组件 */
 let Authorized = RenderAuthorized(getAuthority()); // eslint-disable-line
 
 // Reload the rights component
